460739c0d84f4664a351c33bdfa5d5b0b27d87b6
Basile Bron <basile.bron@protonmail.com>
2023-10-12 12:33:14 +0200

    robustness overwhole + json output

--- trad.py
+++ trad.py
@@ -6,15 +6,30 @@ import math
 import argparse
 import numpy as np
 import pytesseract
-#from googletrans import Translator
-#translator = Translator()
 from PIL import Image, ImageFont, ImageDraw
 import textwrap
 import logging
 import json
-from translate import Translator
+import cProfile
 
-pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+
+#install new llanguage documentation:
+#https://ocrmypdf.readthedocs.io/en/latest/languages.html
+#example :  
+#cd C:\Program Files\Tesseract-OCR\tessdata
+#curl -o chi_sim.traineddata https://github.com/tesseract-ocr/tessdata/raw/main/chi_sim.traineddata
+ 
+
+googletrans = True
+if googletrans:
+  from googletrans import Translator
+  translator = Translator()
+else:
+  #to coment if googletrans
+  from translate import Translator
+  translator= Translator(to_lang="english")
+
+pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
 
 class Blurb(object):
     def __init__(self, x, y, w, h, text, confidence=100.0):
@@ -38,21 +53,20 @@ class Blurb(object):
       self.confidence = confidence
 
     def clean_text(self):
-      """
-      Clean the text content by removing newline characters.
-
-      Returns:
-          str: The cleaned text.
-      """
       text = self.text
+      #Non UTF8 cleaner
+      #if text and text.strip():
+      #  for letter in text:
+      #    if ord(letter) > 255:
+      #        text = text.replace(letter, "")
       text = re.sub(r"\n", "", text)
-      """
-      for letter in text:
 
-          if ord(letter) > 255:
-              #print("deleting atrocity")
-              text = text.replace(letter, "")
-      """
+      # Define a regular expression pattern to allow only specific characters
+      #allowed_characters_pattern = re.compile(r'[a-zA-Z0-9., !?]+')
+      
+      # Keep only allowed characters by applying the regular expression pattern
+      #text = ''.join(allowed_characters_pattern.findall(text))
+
       return text
 
     def __str__(self):
@@ -64,6 +78,20 @@ class Blurb(object):
       """
       return str(self.x) + ',' + str(self.y) + ' ' + str(self.w) + 'x' + str(self.h) + ' ' + str(self.confidence) + '% :' + self.text
 
+def clean_trans_output(text):
+
+  #if text and text.strip():
+  #  for letter in text:
+  #    if ord(letter) > 255:
+  #        text = text.replace(letter, "")
+  # Remove special characters and symbols
+  text = re.sub(r'[^a-zA-Z0-9\s\'-]', '', text)
+
+  # Remove extra spaces and leading/trailing spaces
+  text = ' '.join(text.split())
+
+
+  return text
 
 class TranslatedBlurb(Blurb):
     def __init__(self, x, y, w, h, text, confidence, translation):
@@ -106,13 +134,21 @@ def translate_blurb(blurb):
       Returns:
         blurb (Blurb): The Blurb object translated.
     """
+    
+    try:
+      if googletrans:
+        translated_text = translator.translate(blurb.clean_text(), dest='en')
+        translation = translated_text.text.encode('utf-8', 'ignore')
+      else:
+        translated_text = translator.translate(blurb.clean_text())
+        translation = translated_text.encode('utf-8', 'ignore')
 
-    translated_text = translator.translate(blurb.clean_text())
-    translation = translated_text.encode('utf-8', 'ignore')
+    except Exception as e:
+       logging.error(e)
+       logging.error(blurb.clean_text())
+       translation = ''
 
     return TranslatedBlurb.as_translated(blurb, translation)
-
-
 
 def flow_into_box(text, w, font, min_word_on_line=0.3):
     """
@@ -156,17 +192,17 @@ def flow_into_box(text, w, font, min_word_on_line=0.3):
     if current_line:
         lines.append(" ".join(current_line))
 
-    #print(lines)
     return "\n".join(lines)
 
-def typeset_blurb(img, blurb, transparency):
+def typeset_blurb(img, blurb, transparency :int):
     if isinstance(blurb, TranslatedBlurb):
         text = (blurb.translation.decode("utf-8", 'ignore'))
     else:
         if isinstance(text, bytes):
             text = str(text.decode("utf-8", 'ignore'))
 
-    if len(text) > 4:
+    # Check if there are exactly three words
+    if len(text.split()) > 4:
         area = blurb.w * blurb.h
         fontsize = int(math.sqrt(area) / 10)
 
@@ -179,24 +215,24 @@ def typeset_blurb(img, blurb, transparency):
         lines = textwrap.wrap(text, width=20)  # Adjust the width as needed
 
         if lines:
-            d = ImageDraw.Draw(img)
+          d = ImageDraw.Draw(img)
 
-            # Calculate the height of each line
-            line_height = fontsize + 2  # Adjust as needed
+          # Calculate the height of each line
+          line_height = fontsize + 2  # Adjust as needed
 
-            # Iterate through lines and draw a white box and text for each line
-            for i, line in enumerate(lines):
-                # Calculate the position of the white box for this line
-                box_y = blurb.y + i * line_height
-                box_coords = [(blurb.x, box_y), (blurb.x + blurb.w, box_y + line_height)]
+          # Iterate through lines and draw a white box and text for each line
+          for i, line in enumerate(lines):
+            # Calculate the position of the white box for this line
+            box_y = blurb.y + i * line_height
+            box_coords = [(blurb.x, box_y), (blurb.x + blurb.w, box_y + line_height)]
 
-                # Draw the white box
-                filling = (255, 255, 255, transparency)
-                ImageDraw.Draw(img, "RGBA").rounded_rectangle(box_coords, radius=25, fill=filling, width=0)
+            # Draw the white box
+            filling = (255, 255, 255, transparency)
+            ImageDraw.Draw(img, "RGBA").rounded_rectangle(box_coords, radius=25, fill=filling, width=0)
 
-                # Draw the text on top of the white box
-                text_coords = (blurb.x, box_y)
-                d.text(text_coords, line, fill=(0, 0, 0), font=usingFont, encoding='utf-8')
+            # Draw the text on top of the white box
+            text_coords = (blurb.x, box_y)
+            d.text(text_coords, line, fill=(0, 0, 0), font=usingFont, encoding='utf-8')
 
 def get_params():
     params = ""
@@ -221,7 +257,7 @@ def get_blurbs(img, input_language):
 
   # Convert the input image to grayscale
   img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+  
   # Apply adaptive thresholding to create a binary image (black and white) using a Gaussian method.
   # The thresholded image is then inverted using bitwise_not.
   img_gray = cv2.bitwise_not(cv2.adaptiveThreshold(img_gray, 255, cv2.THRESH_BINARY, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 75, 10))
@@ -289,15 +325,7 @@ def get_blurbs(img, input_language):
       #OCR recognition
       text = pytesseract.image_to_string(pil_image, lang=input_language, config=get_params())
       text = text.replace("\n", "")
-      text = text.replace("\x0c", "")
-      #print(text)
-      #Non UTF8 cleaner
-      """
-      if text and text.strip():
-        for letter in text:
-          if ord(letter) > 255:
-              #print("deleting atrocity")
-              text = text.replace(letter, "")"""
+      text = text.replace("\x0c", "")      
       if text and text.strip() and text != None:
         #filter out noise under x characters
         if len(text)>3:
@@ -306,69 +334,79 @@ def get_blurbs(img, input_language):
           blurbs.append(blurb)
           #print ("Attempt: " + text + ' -> ' + str(translator.translate(text,dest='fr').text))
 
-  return blurbs
+  # Get the dimensions (size) of the input image
+  height, width = img.shape[:2]
+
+  return blurbs, height, width
 
 def load_images_from_folder(folder):
     images = []
-    resize_optimisation = True
-    for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder,filename))
-        if img is not None:
-            if resize_optimisation:
-              # Resize the image to a smaller resolution
-              # You can adjust the target width and height as needed
-              img = cv2.resize(img,(800,1000))
-
-            images.append(img)
-
-    return images
-
-def load_names_from_folder(folder):
     names = []
 
-    for filename in os.listdir(folder):
-        if filename is not None:
-            names.append(filename)
-            print(filename)
+    resize_optimisation = False
+    #for filename in os.listdir(folder):
+    # List of valid image file extensions
+    valid_image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
 
-    return names
+    # Iterate through files in the folder
+    for filename in os.listdir(folder):
+        # Get the file extension of the current file
+        file_extension = os.path.splitext(filename)[1].lower()
+
+        # Check if the file extension is in the list of valid image extensions
+        if file_extension in valid_image_extensions:
+          names.append(filename)
+          img = cv2.imread(os.path.join(folder,filename))
+          if img is not None:
+              if resize_optimisation:
+                # Resize the image to a smaller resolution
+                # You can adjust the target width and height as needed
+                img = cv2.resize(img,(243,358))
+
+              images.append(img)
+    
+    logging.info("filenames: %s", names)
+
+    return images, names
 
 # Configure logging as mentioned in the previous answer
 def log_exception(exc_type, exc_value, exc_traceback):
   # Log the exception with the custom log level "ANY"
   logging.log(100, "An exception occurred", exc_info=(exc_type, exc_value, exc_traceback))
 
-# Set the custom exception handler
-sys.excepthook = log_exception
 
-if __name__ == "__main__":
-  #str = str.encode('utf-8', errors='ignore').decode('utf-8')
 
-  logging.basicConfig(
-    level=logging.DEBUG,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-    filename='trad.log',  # Specify a log file
-    format='%(asctime)s [%(levelname)s]: %(message)s',  # Define the log message format
-    datefmt='%Y-%m-%d %H:%M:%S'  # Define the date format
-  )
+
+
+
+
+def main():
+
+  logging.debug('Script starting')
 
   # Initialize parser
   parser = argparse.ArgumentParser(description="Your script description here")
 
   # Add required argument for input language
-  parser.add_argument("-i", "--input_language", help="Input language ('jpn_vert' for Japanese vertical) more at: https://tesseract-ocr.github.io/tessdoc/Data-Files-in-different-versions.html", choices=['jpn_vert'], required=True)
-
-  # Add optional argument for output language with a default of 'fr'
-  parser.add_argument("-o", "--output_language", help="Output language ('fr' by default)", choices=['fr'], default='fr')
-
-  # Add optional arguments with default values
+  parser.add_argument("-i", "--input_language", help="Input language ('jpn_vert' for Japanese vertical) more at: https://tesseract-ocr.github.io/tessdoc/Data-Files-in-different-versions.html", default='jpn_vert')
+  parser.add_argument("-o", "--output_language", help="Output language ('en' by default)",type=str, default='en')
   parser.add_argument("-if", "--input_folder", help="Input folder path", default="./original")
   parser.add_argument("-of", "--output_folder", help="Output folder path", default="./translated")
   parser.add_argument("-t", "--transparency", help="Transparency (between 1 and 255)", type=int, default=200)
   parser.add_argument("-j", "--json", help="return only the translated text", type=bool, default=True)
 
-  # Parse the command line arguments
-  args = parser.parse_args()
+  try:
+    args = parser.parse_args()
+    logging.debug(args)
+  except argparse.ArgumentError as e:
+    # Log the specific argument that caused the error
+    logging.error(f"Invalid argument: {e.argument_name}")
+    # Log the received arguments
+    logging.debug(f"Received arguments: {sys.argv[1:]}")
+  except Exception as e:
+    logging.error(f"An error occurred while parsing arguments: {e}")
 
+    
   # Access the values of the arguments
   input_folder = args.input_folder
   output_folder = args.output_folder
@@ -376,6 +414,26 @@ if __name__ == "__main__":
   input_language = args.input_language
   output_language = args.output_language
   output_json = args.json
+
+  # Define a mapping of language aliases to Tesseract language codes
+  language_mapping = {
+      'chi_sim': 'chi_sim_vert',
+      'chinese': 'chi_sim_vert',
+      'china': 'chi_sim_vert',
+      'japanese': 'jpn_vert',
+      'japan': 'jpn_vert',
+      # Add more aliases and language codes as needed
+  }
+
+  # Get the user input for the language parameter
+  user_input = input_language.strip().lower()
+
+  # Use the mapping to determine the Tesseract language code
+  if user_input in language_mapping:
+      input_language = language_mapping[user_input]
+  else:
+      # Default to 'eng' (English) if the input doesn't match any alias
+      input_language = 'jpn_vert'
 
   # Now you can use these values in your script
   logging.info("Input folder path: %s", input_folder)
@@ -387,10 +445,11 @@ if __name__ == "__main__":
 
   # Check if input_folder is a folder or a single image file
   if os.path.isdir(input_folder):
+      logging.info("input path is a folder")
       # Load images from the folder
-      images = load_images_from_folder(input_folder)
-      names = load_names_from_folder(input_folder)
+      images, names = load_images_from_folder(input_folder)
   elif os.path.isfile(input_folder):
+      logging.info("input path is a file")
       # Input is a single image file
       img = cv2.imread(os.path.join(input_folder))
       if img is not None:
@@ -414,67 +473,89 @@ if __name__ == "__main__":
       with open(output_json_file, "r", encoding="utf-8") as json_file:
         try:
           translations = json.load(json_file)
-        except :
-           pass
+        except Exception as e:
+            # Handle JSON loading errors here
+            logging.error(f"Error loading JSON file: {str(e)}")
   i = 0
   b = 0
 
-  # Initialize the translator for Japanese to French (or your desired target language)
-  translator = Translator(from_lang='ja', to_lang=output_language)
-
   for img in images:
-    blurbs = get_blurbs(img, input_language)
-    needTransImg = Image.fromarray(img.copy())
+    image_filename = names[i]
+    
+    # Check if already translated
+    if image_filename not in translations:
+      blurbs, height, width = get_blurbs(img, input_language)
 
-    for blurb in blurbs:
-      logging.debug("blurb number : %s", b)
-      logging.debug("out of : %s", len(blurbs))
+      for blurb in blurbs:
+        logging.debug("blurb number : %s", b)
+        logging.debug("out of : %s", len(blurbs))
 
-      b += 1
+        b += 1
+        if output_json:
+          # Translate the blurb and store the translation information in the dictionary
+          translated = translate_blurb(blurb)
+          text = clean_trans_output(str(translated.translation.decode("utf-8", 'ignore')))
+          if translated.translation != '' and len(text.split()) > 3:
+            translation_info = {
+                "x": round(float(translated.x / width), 2),
+                "y": round(float(translated.y / height), 2),
+                "w": round(float(translated.w / width), 2),
+                "h": round(float(translated.h / height), 2),
+                #"confidence": translated.confidence,
+                #"original_text": str(blurb.text),
+                "translated_text": text,
+                "num_char": len(text)
+            }
+
+            # Use the image filename as a unique key for each image in the translations dictionary
+            if image_filename not in translations:
+              try:
+                translations[image_filename] = []
+              except:
+                translations[image_filename] = translation_info
+                
+            translations[image_filename].append(translation_info)
+        else:
+          needTransImg = Image.fromarray(img.copy())
+          typeset_blurb(needTransImg, translated, transparency)
+          trans = needTransImg
+
       if output_json:
-        # Translate the blurb and store the translation information in the dictionary
-        translated = translate_blurb(blurb)
-        translation_info = {
-            "x": translated.x,
-            "y": translated.y,
-            "w": translated.w,
-            "h": translated.h,
-            #"confidence": translated.confidence,
-            #"original_text": blurb.text,
-            "translated_text": translated.translation.decode("utf-8", 'ignore')
-        }
-
-        # Use the image filename as a unique key for each image in the translations dictionary
-        image_filename = names[i]
-        if image_filename not in translations:
-          translations[image_filename] = []
-
-        translations[image_filename].append(translation_info)
-        
+        # Save the updated translations to the JSON file
+        with open(output_json_file, "w", encoding="utf-8") as json_file:
+          logging.debug(translations)
+          try:
+            # Attempt to serialize the translation_info to JSON
+            json.dump(translations, json_file, ensure_ascii=False, indent=4)
+          except Exception as e:
+            # Handle the exception, print debugging information, or remove the problematic data
+            logging.error(str(e))
       else:
-        typeset_blurb(needTransImg, translated, transparency)
-        trans = needTransImg
-
-      typeset_blurb(needTransImg, translated, transparency)
-      trans = needTransImg
-
-    if output_json:
-      # Save the updated translations to the JSON file
-      with open(output_json_file, "w", encoding="utf-8") as json_file:
-        logging.info(translations)
-        try:
-          # Attempt to serialize the translation_info to JSON
-          json.dump(str(translations), json_file, ensure_ascii=False, indent=4)
-        except Exception as e:
-          # Handle the exception, print debugging information, or remove the problematic data
-          logging.info(str(e))
-
-    else:
-      # Save the translated image with the image filename as the name
-      save_file = os.path.join(output_folder, str(i) + '.jpg')
-      logging.info(save_file)
-      trans.save(save_file, format='JPEG')
+        # Save the translated image with the image filename as the name
+        save_file = os.path.join(output_folder, str(i) + '.jpg')
+        logging.info(save_file)
+        trans.save(save_file, format='JPEG')
     i += 1
     
+    logging.debug('Already translated: '+ image_filename)
+
+
   print('Number of translated blurb:' + str(b))
   print('Number of translated pages:' + str(i))
+
+  logging.debug('Script end')
+
+if __name__ == "__main__":
+
+  logging.basicConfig(
+    level=logging.INFO,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    filename='trad.log',  # Specify a log file
+    format='%(asctime)s [%(levelname)s]: %(message)s',  # Define the log message format
+    datefmt='%Y-%m-%d %H:%M:%S'  # Define the date format
+  )
+
+  # Set the custom exception handler
+  sys.excepthook = log_exception
+
+  main()
+  #cProfile.run('main()')
